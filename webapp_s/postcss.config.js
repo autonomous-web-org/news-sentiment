4568054c6ae56e37044384282f02f0a3b5d53612
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-10-08 21:37:12 +0530

    static removed

--- webapp_s/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),  // Tailwind v4 PostCSS plugin :contentReference[oaicite:1]{index=1}
-    require('autoprefixer')           // autoprefixer for vendor prefixes :contentReference[oaicite:2]{index=2}
-  ]
-}
